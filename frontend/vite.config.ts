9561ef11b0e5d51e7278106cdcb700d6cc687e04
Prestgg1 <prestgg12@gmail.com>
2026-04-21 10:17:30 +0400

    added netlify supper

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter(),
+  netlifyReactRouter()
+
+  ],
   resolve: {
     tsconfigPaths: true,
   },
-  server : {
-    allowedHosts : ['tapz3h-ip-37-114-131-126.tunnelmole.net']
+  server: {
+    allowedHosts: ['tapz3h-ip-37-114-131-126.tunnelmole.net']
   }
 });
