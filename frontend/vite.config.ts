da91874094c8a45fd3ab1ebf1a9675534fe8f113
Prestgg1 <prestgg12@gmail.com>
2026-04-21 10:31:44 +0400

    Vite config updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(),
-  netlifyReactRouter()
-
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    netlifyReactRouter(),
+    tsconfigPaths(), // ← plugin kimi işlət
   ],
-  resolve: {
-    tsconfigPaths: true,
-  },
   server: {
     allowedHosts: ['tapz3h-ip-37-114-131-126.tunnelmole.net']
   }
