673a2b53dcd912458ecb988515b0d103ceb6b43a
Isa Mammadli <i.a.memmedli@gmail.com>
2026-06-21 19:21:50 +0400

    fix

--- backend/src/modules/contact-messages/contact-messages.module.ts
+++ backend/src/modules/contact-messages/contact-messages.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContactMessage } from './entities/contact-message.entity';
 import { ContactMessagesService } from './contact-messages.service';
 import { ContactMessagesController } from './contact-messages.controller';
+import { AuthModule } from '../auth/auth.module';
+import { AdminGuard } from '../../guards/admin.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ContactMessage])],
-  providers: [ContactMessagesService],
+  imports: [TypeOrmModule.forFeature([ContactMessage]), AuthModule],
+  providers: [ContactMessagesService, AdminGuard],
   controllers: [ContactMessagesController],
   exports: [ContactMessagesService],
 })
--- backend/src/modules/site-content/site-content.module.ts
+++ backend/src/modules/site-content/site-content.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SiteSetting } from './entities/site-setting.entity';
 import { SiteContentService } from './site-content.service';
 import { SiteContentController } from './site-content.controller';
+import { AuthModule } from '../auth/auth.module';
+import { AdminGuard } from '../../guards/admin.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SiteSetting])],
-  providers: [SiteContentService],
+  imports: [TypeOrmModule.forFeature([SiteSetting]), AuthModule],
+  providers: [SiteContentService, AdminGuard],
   controllers: [SiteContentController],
   exports: [SiteContentService],
 })
